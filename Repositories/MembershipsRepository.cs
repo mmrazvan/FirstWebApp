a8354f9c624a1a5c66c3ee647b4d88a5c9682ee6
Razvan Mihalca <mmrazvan41@gmail.com>
2023-03-02 15:26:18 +0200

    20230302 added CRUD to Membership Repository

--- Repositories/MembershipsRepository.cs
+++ Repositories/MembershipsRepository.cs
@@ -15,5 +15,30 @@ namespace FirstMVCApp.Repositories
 		{
 			return _context.Memberships;
 		}
+
+		public MembershipModel GetMember(Guid id)
+		{
+			MembershipModel model = _context.Memberships.FirstOrDefault(x => x.IDMembership == id);
+			return model;
+		}
+
+		public void AddMember(MembershipModel membershipModel)
+		{
+			membershipModel.IDMembership = Guid.NewGuid();
+			_context.Memberships.Add(membershipModel);
+			_context.SaveChanges();
+		}
+		public void Update(MembershipModel membershipModel
+			)
+		{
+			_context.Memberships.Update(membershipModel);
+			_context.SaveChanges();
+		}
+		public void Delete(Guid id)
+		{
+			MembershipModel membershipModel = GetMember(id);
+			_context.Memberships.Remove(membershipModel);
+			_context.SaveChanges();
+		}
 	}
 }
